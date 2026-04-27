8617d79b601fed7777b5ceb76dc2dc9b62f2229b
nenshibudheliya-coder <nenshibudheliya@gmail.com>
2026-04-27 18:18:56 +0530

    create levels

--- src/data/levels.js
+++ src/data/levels.js
@@ -300,5 +300,121 @@ export const LEVELS = [
             { color: "green", pos: [4, 1] },
         ]
     },
+    {
+        name: "Level 21",
+        size: 5,
+        dots: [
+            { color: "green", pos: [0, 0] },
+            { color: "green", pos: [3, 4] },
+            { color: "yellow", pos: [1, 3] },
+            { color: "yellow", pos: [2, 2] },
+            { color: "blue", pos: [1, 4] },
+            { color: "blue", pos: [2, 1] },
+            { color: "red", pos: [2, 4] },
+            { color: "red", pos: [3, 1] },
+        ]
+    },
+    {
+        name: "Level 22",
+        size: 5,
+        dots: [
+            { color: "orange", pos: [0, 0] },
+            { color: "orange", pos: [0, 4] },
+            { color: "blue", pos: [1, 1] },
+            { color: "blue", pos: [3, 0] },
+            { color: "yellow", pos: [1, 4] },
+            { color: "yellow", pos: [2, 1] },
+            { color: "red", pos: [2, 4] },
+            { color: "red", pos: [4, 0] },
+            { color: "green", pos: [3, 4] },
+            { color: "green", pos: [4, 2] },
 
+        ]
+    },
+    {
+        name: "Level 23",
+        size: 5,
+        dots: [
+            { color: "yellow", pos: [0, 0] },
+            { color: "yellow", pos: [0, 4] },
+            { color: "blue", pos: [1, 0] },
+            { color: "blue", pos: [3, 1] },
+            { color: "green", pos: [1, 4] },
+            { color: "green", pos: [4, 3] },
+            { color: "red", pos: [2, 2] },
+            { color: "red", pos: [4, 2] },
+        ]
+    },
+    {
+        name: "Level 24",
+        size: 5,
+        dots: [
+            { color: "yellow", pos: [1, 1] },
+            { color: "yellow", pos: [1, 3] },
+            { color: "blue", pos: [2, 0] },
+            { color: "blue", pos: [2, 2] },
+            { color: "green", pos: [2, 1] },
+            { color: "green", pos: [3, 3] },
+            { color: "red", pos: [3, 0] },
+            { color: "red", pos: [3, 4] },
+        ]
+    },
+    {
+        name: "Level 25",
+        size: 5,
+        dots: [
+            { color: "yellow", pos: [0, 3] },
+            { color: "yellow", pos: [1, 0] },
+            { color: "orange", pos: [0, 4] },
+            { color: "orange", pos: [1, 1] },
+            { color: "red", pos: [2, 0] },
+            { color: "red", pos: [4, 2] },
+            { color: "green", pos: [2, 1] },
+            { color: "green", pos: [4, 3] },
+            { color: "blue", pos: [3, 1] },
+            { color: "blue", pos: [3, 3] },
+        ]
+    },
+    {
+        name: "Level 26",
+        size: 5,
+        dots: [
+            { color: "green", pos: [0, 0] },
+            { color: "green", pos: [4, 2] },
+            { color: "blue", pos: [0, 2] },
+            { color: "blue", pos: [4, 3] },
+            { color: "yellow", pos: [1, 0] },
+            { color: "yellow", pos: [3, 0] },
+            { color: "red", pos: [2, 2] },
+            { color: "red", pos: [4, 0] },
+        ]
+    },
+    {
+        name: "Level 27",
+        size: 5,
+        dots: [
+            { color: "yellow", pos: [1, 1] },
+            { color: "yellow", pos: [1, 3] },
+            { color: "blue", pos: [1, 4] },
+            { color: "blue", pos: [2, 2] },
+            { color: "green", pos: [2, 3] },
+            { color: "green", pos: [3, 1] },
+            { color: "red", pos: [2, 4] },
+            { color: "red", pos: [3, 0] },
+        ]
+    },
+    {
+        name: "Level 28",
+        size: 5,
+        dots: [
+            { color: "yellow", pos: [0, 2] },
+            { color: "yellow", pos: [1, 0] },
+            { color: "green", pos: [0, 3] },
+            { color: "green", pos: [3, 1] },
+            { color: "blue", pos: [1, 1] },
+            { color: "blue", pos: [2, 3] },
+            { color: "red", pos: [3, 3] },
+            { color: "red", pos: [4, 0] },
+        ]
+    },
 ];
